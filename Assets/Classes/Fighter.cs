6d776d5d7af9fc77ac8b68a7ee85e752d4ed1527
AlexPhill <alexander.phillips@ontariotechu.net>
2022-10-26 15:34:36 -0400

    Added new scripts for classes & prefabs
    also added enemy sprite/prefab

---
+++ Assets/Classes/Fighter.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fighter : Character
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = 25;
+        maxHP = 25;
+        ATK = 4;
+        DEF = 3;
+        MAG = 0;
+        gHP = 4;
+        gATK = 4;
+        gMAG = 0;
+        gDEF = 3;
+        AGL = 40;
+        LUK = 40;
+        magicClass = false;
+        attackMult = 100;
+        targetAlly = true;
+}
+
+    public override void Ability(Character target)
+    {
+        target.attackUp +=0.5f;
+    }
+
+
+
+
+}
---
+++ Assets/Classes/Lancer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lancer : Character
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = 20;
+        ATK = 3;
+        DEF = 6;
+        MAG = 2;
+        gHP = 6;
+        gATK = 3;
+        gMAG = 2;
+        gDEF = 6;
+        AGL = 20;
+        LUK = 20;
+        magicClass = false;
+        attackMult = 60;
+        targetAlly = false;
+    }
+
+    public override void Ability(Character target)
+    {
+        target.isAggroed=true;
+    }
+}
